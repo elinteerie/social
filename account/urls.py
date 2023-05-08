f013b014026e0da5b4c437d254b9b26d6ce3a4af
elinteerie <elinteerie@gmail.com>
2023-05-08 16:49:04 +0100

    first commit

---
+++ account/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import register, dashboard, edit
+
+#app_name = 'account'
+
+urlpatterns = [
+    #path('login/', views.user_login, name = 'login'),
+    # path('login/', auth_views.LoginView.as_view(), name ='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name ='logout'),
+    path('', include('django.contrib.auth.urls')),
+    path('', dashboard, name = 'dashboard'),
+    path('register/', register, name = 'register'),
+    path('edit/', edit, name = 'edit'),
+    
+    #change password urls
+    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # path('password-reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
+    # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
